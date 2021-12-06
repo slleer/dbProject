ee63d046acb833a8bffa8ad8ddc5c968d4cab233
Stephen Leer <stephen.leroy.leer@gmail.com>
2021-12-06 10:45:45 -0800

    updates
    updates

--- dbms/PA4_A1.sql
+++ dbms/PA4_A1.sql
@@ -6,8 +6,8 @@
 CREATE DATABASE CS457_PA4;
 USE CS457_PA4;
 create table Flights (seat int, status int);
-insert into Flights values (22,0); -- seat 22 is available
-insert into Flights values (23,1); -- seat 23 is occupied
+insert into Flights values (22,0);
+insert into Flights values (23,1);
 begin transaction;
 update flights set status = 1 where seat = 22;
 
--- dbms/PA4_B1.sql
+++ dbms/PA4_B1.sql
@@ -8,6 +8,6 @@ USE CS457_PA4;
 select * from Flights;
 begin transaction;
 update flights set status = 1 where seat = 22;
-commit; --there should be nothing to commit; it's an "abort"
+commit;
 select * from Flights;
 
